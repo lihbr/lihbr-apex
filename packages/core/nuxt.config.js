b7f77f7d270754db826a1a6874b4a07c1c414eac
lihbr <lihbr@users.noreply.github.com>
2021-04-11 03:11:22 +0200

    revert(core): keep verbose log for ci

--- packages/core/nuxt.config.js
+++ packages/core/nuxt.config.js
@@ -37,14 +37,10 @@ module.exports = async () => {
 
   // Logging
   logger.info(`Datalayer content fetched in ${dlEndTime - dlStartTime} ms`);
-  if (ci.isNetlify()) {
-    logger.info(`${dlRoutes.length} routes found`);
-  } else {
-    logger.info(
-      "Found routes:\n",
-      dlRoutes.map(i => i.route || i)
-    );
-  }
+  logger.info(
+    "Found routes:\n",
+    dlRoutes.map(i => i.route || i)
+  );
 
   return {
     /*
