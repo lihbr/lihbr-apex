74fb1cb216a0809c00101ca48c40563490fa4a13
lihbr <lihbr@users.noreply.github.com>
2021-11-21 15:19:46 +0100

    feat: update style

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,52 +36,56 @@ module.exports = {
 			transparent: "transparent",
 			current: "currentColor",
 			inherit: "inherit",
+			theme: {
+				DEFAULT: "var(--color-theme)",
+				100: "var(--color-theme-100)",
+			},
 			slate: {
-				DEFAULT: "#1e1919", // 800
+				DEFAULT: "#1f1919", // 800
 				900: "#131010",
-				700: "#292222",
-				600: "#342b2b",
-				100: "#6a5959",
-				50: "#806b6b",
+				700: "#2c2323",
+				200: "#7c6565",
+				100: "#8e7878",
+				50: "#a18e8e",
 			},
 			cream: {
-				DEFAULT: "#fff7f7", // 800
+				DEFAULT: "#faf1f1", // 800
 				900: "#fffefe",
-				700: "#ffe9e9",
-				600: "#ffdbdb",
-				100: "#ff9595",
-				50: "#ff8787",
+				700: "#f2e4e4",
+				200: "#b4a4a4",
+				100: "#ab9a9a",
+				50: "#a69191",
 			},
 			// o-20 used for tap highlight and inline code only
 			navy: {
 				DEFAULT: "#54669c",
 				"o-20": alpha("#54669c", 0.2),
-				100: "#7284ba",
+				100: "#8a98c3",
 			},
 			beet: {
 				DEFAULT: "#a54a5e",
 				"o-20": alpha("#a54a5e", 0.2),
-				100: "#c3687c",
+				100: "#cc7e8f",
 			},
 			flamingo: {
 				DEFAULT: "#e84311",
 				"o-20": alpha("#e84311", 0.2),
-				100: "#ff6129",
+				100: "#fc693c",
 			},
 			ochre: {
 				DEFAULT: "#f27502",
 				"o-20": alpha("#f27502", 0.2),
-				100: "#ff9320",
+				100: "#ff922d",
 			},
 			butter: {
 				DEFAULT: "#ffb005",
 				"o-20": alpha("#ffb005", 0.2),
-				100: "#ffce23",
+				100: "#ffbf34",
 			},
 			mantis: {
 				DEFAULT: "#759f53",
 				"o-20": alpha("#759f53", 0.2),
-				100: "#93bd71",
+				100: "#a4c589",
 			},
 		},
 		extend: {
