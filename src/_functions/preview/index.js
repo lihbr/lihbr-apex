6ef01a43ce87e0b6fbde249e56de3f9dd94b06d7
lihbr <lihbr@users.noreply.github.com>
2022-01-10 22:03:34 +0100

    fix: set `APP_URL` dynamically in serverless

--- src/_functions/preview/index.js
+++ src/_functions/preview/index.js
@@ -5,12 +5,21 @@ const { prismicPluginOptions } = require("./eleventy.config.js");
 require("./eleventy-bundler-modules.js");
 
 const handler = async (event) => {
-	return await prismicPreview.handle(
+	// Set APP_URL dynamically
+	if (process.env.AWS_LAMBDA_FUNCTION_NAME && event.headers?.host) {
+		process.env.APP_URL = `https://${event.headers.host}`;
+	}
+
+	const response = await prismicPreview.handle(
 		event.path,
 		event.queryStringParameters,
 		event.headers,
 		prismicPluginOptions,
 	);
+
+	delete process.env.APP_URL;
+
+	return response;
 };
 
 exports.handler = handler;
