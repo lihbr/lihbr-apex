3c8de445e826ca14661d6deb403b49d3e46b74c1
lihbr <lihbr@users.noreply.github.com>
2022-12-08 23:03:28 +0100

    fix: resolve links for `post__document`

--- src/_11ty/prismic/linkResolver.ts
+++ src/_11ty/prismic/linkResolver.ts
@@ -16,6 +16,9 @@ export const linkResolver: LinkResolverFunction = (doc) => {
 		case "post__blog":
 			return `/blog/${doc.uid}`;
 
+		case "post__document":
+			return `/${doc.uid}`;
+
 		case "__eleventy":
 			return (doc.url ?? "/").replace(".html", "");
 
