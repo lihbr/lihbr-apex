f20043af1a2a1e510f1a2c24fdc432c92f71bd72
Lucie <25330882+lihbr@users.noreply.github.com>
2022-12-17 17:33:43 +0100

    chore: add `rel` attributes to social links

--- packages/core/src/components/partials/footer/Socials.vue
+++ packages/core/src/components/partials/footer/Socials.vue
@@ -13,6 +13,7 @@
           :title="social.type"
           :blank="social.link.blank"
           :description="social.description"
+          :rel="{ social.type.toLowerCase() === 'github' ? 'me authn' : 'me' }"
           large
         >
           <component
