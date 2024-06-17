9360eb1073f98be181e9030c3e41bb23584a4ed5
lihbr <lihbr@users.noreply.github.com>
2024-06-17 18:32:42 +0200

    feat: preview private pages

--- src/functions/preview/preview.akte.app.ts
+++ src/functions/preview/preview.akte.app.ts
@@ -5,11 +5,12 @@ import type { GlobalData } from "../../akte/types"
 import { preview } from "../../files/preview"
 import { fourOFour } from "../../files/404"
 import { slug } from "../../files/[slug]"
+import { slug as privateSlug } from "../../files/private/[slug]"
 import { slug as postsSlug } from "../../files/posts/[slug]"
 import { index as art } from "../../files/art/index"
 
 export const app = defineAkteApp<GlobalData>({
-	files: [preview, fourOFour, slug, postsSlug, art],
+	files: [preview, fourOFour, slug, privateSlug, postsSlug, art],
 	globalData() {
 		return {}
 	},
